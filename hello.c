22a38eb558856c47b3d637fbf83f2c62019233da
Gomati <gkarampudi2@huskers.unl.edu>
2022-09-22 22:15:21 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ *
+ * Author: Gomati Karampudi
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Gomati Karampudi!\n");
+  printf("Computer Science!\n");
+
+  return 0;
+}
